1c57dbb586531d81f26699b3139071f6ffdf9f67
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-04 20:26:22 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/ungdungdangonngu1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/ungdungdangonngu1903/MainActivity.java
@@ -1,13 +1,22 @@
 package khoapham.ptp.phamtanphat.ungdungdangonngu1903;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableRow;
+
+import khoapham.ptp.phamtanphat.ungdungdangonngu1903.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
+        user = new User("Đăng nhập hệ thống","Nhập tài khoản" , "Nhập mật khẩu" , "Đăng nhập");
+
+
+
     }
 }
---
+++ app/src/main/java/khoapham/ptp/phamtanphat/ungdungdangonngu1903/User.java
@@ -0,0 +1,61 @@
+package khoapham.ptp.phamtanphat.ungdungdangonngu1903;
+
+import android.databinding.BaseObservable;
+import android.databinding.Bindable;
+
+import com.android.databinding.library.baseAdapters.BR;
+
+
+public class User extends BaseObservable {
+    private String title;
+    private String hintuser;
+    private String hintpassword;
+    private String textbutton;
+
+    public User(String title, String hintuser, String hintpassword, String textbutton) {
+        this.title = title;
+        this.hintuser = hintuser;
+        this.hintpassword = hintpassword;
+        this.textbutton = textbutton;
+    }
+
+    @Bindable
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+        notifyPropertyChanged(BR.title);
+    }
+
+    @Bindable
+    public String getHintuser() {
+        return hintuser;
+    }
+
+    public void setHintuser(String hintuser) {
+        this.hintuser = hintuser;
+        notifyPropertyChanged(BR.hintuser);
+    }
+
+    @Bindable
+    public String getHintpassword() {
+        return hintpassword;
+    }
+
+    public void setHintpassword(String hintpassword) {
+        this.hintpassword = hintpassword;
+        notifyPropertyChanged(BR.hintpassword);
+    }
+
+    @Bindable
+    public String getTextbutton() {
+        return textbutton;
+    }
+
+    public void setTextbutton(String textbutton) {
+        this.textbutton = textbutton;
+        notifyPropertyChanged(BR.textbutton);
+    }
+}
