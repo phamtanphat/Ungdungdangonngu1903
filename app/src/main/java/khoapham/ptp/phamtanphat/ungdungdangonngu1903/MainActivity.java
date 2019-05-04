59753fe348ee0c39782951442fd85ca11d6cb61a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-05-04 20:54:08 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/ungdungdangonngu1903/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/ungdungdangonngu1903/MainActivity.java
@@ -3,6 +3,8 @@ package khoapham.ptp.phamtanphat.ungdungdangonngu1903;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.TableRow;
 
 import khoapham.ptp.phamtanphat.ungdungdangonngu1903.databinding.ActivityMainBinding;
@@ -13,10 +15,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityMainBinding mainBinding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
+        final ActivityMainBinding mainBinding = DataBindingUtil.setContentView(MainActivity.this,R.layout.activity_main);
         user = new User("Đăng nhập hệ thống","Nhập tài khoản" , "Nhập mật khẩu" , "Đăng nhập");
+        mainBinding.setUser(user);
+        mainBinding.executePendingBindings();
 
-
-
+        mainBinding.buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String user = mainBinding.edittextUsername.getText().toString();
+                String password = mainBinding.edittextPassword.getText().toString();
+                if (!TextUtils.isEmpty(user) && password.length() > 0){
+                    mainBinding.textviewInfo.setText("Tài khoản : " + user + "\n" +"Mật khẩu : " + password );
+                }
+            }
+        });
     }
 }
